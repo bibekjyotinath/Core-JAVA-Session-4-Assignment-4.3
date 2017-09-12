be51ee068c71493296f9ec2d088629186eafca4a
bibekjyotinath <bibekjyotinath92@gmail.com>
2017-09-12 18:58:10 +0530

    Assignment 4.3
    Assignment 4.3

--- Assignment 4.3/src/FinalDemo.java
+++ Assignment 4.3/src/FinalDemo.java
@@ -1,35 +1,33 @@
-import java.util.*;
-class Age{
-	final int age;
-	public Age() {
-		age = 18;
+import java.util.*;    //importing util class to use scanner method
+class Age{		//class age creation
+	final int age;	//final variable created in integer type
+	public Age() {		//constructor of Age class 
+		age = 18;	//assigning value to age
 	}
 }
-final class Check extends Age{
-    public final void Checkdata(int check_age) {
-		if(age == check_age) {
-			System.out.println("The age matches!");
-			//return 0;
+final class Check extends Age{		//creation of final class Check which is extended by the age class 
+    public final void Checkdata(int check_age) {    //creation of final method to chech the age 
+		if(age == check_age) {		//checks if the age passed is equal to the age assigned
+			System.out.println("The age matches!");   //if true prints a message on sonsole as matches
 		}
 		else {
-			System.out.println("The age doesnot match!");
-			//return 1;
+			System.out.println("The age doesnot match!");   //if not prints a message on console as doesnt match
 		}
 	}
 }
-public class FinalDemo {
+public class FinalDemo {		//main class holding the main method of the program	
 
-	public static void main(String[] args) {
+	public static void main(String[] args) {    //Main method of the program
 		
-		int check_age;
+		int check_age;		//intialization of variable in integer type
 		
-		Scanner sc = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);   //creating object of scanner class 
 		
-		Check chk = new Check();
-		System.out.println("Enter Age: ");
-		check_age = sc.nextInt();
-		chk.Checkdata(check_age);
-		sc.close();
+		Check chk = new Check();     //creating object of class check
+		System.out.println("Enter Age: ");  //Prints message to get input from user
+		check_age = sc.nextInt();	//stores the value to the variable check_age using scanner object
+		chk.Checkdata(check_age);	//calling of mehod Checkdata using the object of Check class
+		sc.close();	//closing the scanner method 
 		
 	}
 
